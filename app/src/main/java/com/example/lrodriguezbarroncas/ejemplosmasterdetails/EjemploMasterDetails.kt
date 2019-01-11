cddbc3b7974f8301e437b29271123e3f644bf4ea
Luisfrb56 <luisfrb56@gmail.com>
2019-01-11 10:11:06 +0100

    Primer commit, con los dos layouts hechos

---
+++ app/src/main/java/com/example/lrodriguezbarroncas/ejemplosmasterdetails/EjemploMasterDetails.kt
@@ -0,0 +1,34 @@
+package com.example.lrodriguezbarroncas.ejemplosmasterdetails
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_ejemplo_master_details.*
+import android.widget.Toast
+
+
+
+class EjemploMasterDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejemplo_master_details)
+
+        var Horizontal=false
+        val mensa :Toast
+
+        if ( Vertical!= null) {
+             Horizontal= true
+            mensa = Toast.makeText(applicationContext, "Horizontal", Toast.LENGTH_SHORT)
+            mensa.show()
+        }else{
+            mensa=Toast.makeText(applicationContext, "Vertical", Toast.LENGTH_SHORT)
+            mensa.show()
+        }
+
+
+    }
+
+
+
+
+}
